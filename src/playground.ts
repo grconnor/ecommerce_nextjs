b8b788f1022245621ce7b3ae59a41dfe68b0d11f
Connor Roelofsen <connorroelofsen15@gmail.com>
2023-09-10 09:38:24 +0200

    Mapped types, array index types

--- src/playground.ts
+++ src/playground.ts
@@ -2,37 +2,24 @@ interface Person {
   name: string;
 }
 
-interface Student extends Person {
-  age: number;
-}
+type CustomArray<T> = {
+  [index: number]: T;
+};
 
-interface PostGraduateStudent extends Person {
-  age: number;
-  projects: string[];
-}
-
-type StudentInfo<T extends any = Student> = T extends Student
-  ? {
-      data: T;
-      grades: number[];
-    }
-  : string;
-
-type Car = { engine: string};
+type CustomObject<T = string | number | Person> = {
+  [key: string]: T;
+};
 
 export default function play() {
-  function logStudentInfo(info: StudentInfo<Car>) {
-    console.log(info);
-    console.log(info);
-  }
+  const items: CustomArray<string> = ["1", "2", "3"];
+  const items2: CustomArray<number> = [1, 2, 3];
 
-  const info = {
-    data: {
-      name: "Connor",
-      age: 30,
+  const person: CustomObject = {
+    age: 23,
+    name: "Connor",
+    city: "Stockholm",
+    person: {
+      name: "John",
     },
-    grades: [1, 2, 3],
   };
-
-  logStudentInfo(info);
 }
