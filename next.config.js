d5dbc548af1e4c620689cd8472cd23cfd476a4b5
Connor Roelofsen <connorroelofsen15@gmail.com>
2023-09-23 18:33:02 +0200

    Shopify config, create mock getProducts for Bigcommerce framework, install prettier and format json in configs, create error handling for framework configs, reusable types for api, implement api types in Shopify getAllProducts component as well as in Shopify fetchAPI.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
 const { withFrameworkConfig } = require("./src/framework/common/config");
 
-const nextConfig = withFrameworkConfig({
-  reactStrictMode: true,
-  i18n: {
-    locales: ["en-US"],
-    defaultLocale: "en-US",
-  },
-});
+async function getNextConfig() {
+  const nextConfig = await withFrameworkConfig({
+    framework: {
+      name: "shopify_local",
+    },
+    reactStrictMode: true,
+    i18n: {
+      locales: ["en-US"],
+      defaultLocale: "en-US",
+    },
+  });
 
-module.exports = nextConfig;
+  return nextConfig;
+}
 
-console.log("next.config.js", JSON.stringify(module.exports, null, 2));
+module.exports = async () => {
+  const config = await getNextConfig();
+  console.log("Next.config.js: ", JSON.stringify(config, null, 2));
+  return config;
+};
--- src/framework/common/config.js
+++ src/framework/common/config.js
@@ -1,15 +1,35 @@
 const fs = require("fs");
 const path = require("path");
 const merge = require("deepmerge");
+const prettier = require("prettier");
 
-function withFrameworkConfig(defaultConfig = {}) {
-  const framework = "shopify";
+const allowedFrameworks = ["shopify", "bigcommerce", "shopify_local"];
+const fallbackFramework = allowedFrameworks[0];
 
-  const frameworkNextConfig = require(path.join(
-    "../",
-    framework,
-    "next.config"
-  ));
+async function withFrameworkConfig(defaultConfig = {}) {
+  let framework = defaultConfig?.framework?.name;
+
+  if (!framework) {
+    throw new Error(
+      "The API framework is missing, please add a vaid provider!"
+    );
+  }
+
+  if (!allowedFrameworks.includes(framework)) {
+    throw new Error(
+      `The api framework: '${framework}' cannot be found, please use one of the following: ${allowedFrameworks.join(
+        ", "
+      )}.`
+    );
+  }
+
+  if (framework === "shopify_local") {
+    framework = fallbackFramework;
+  }
+
+  const frameworkNextConfig = require(
+    path.join("../", framework, "next.config")
+  );
   const config = merge(defaultConfig, frameworkNextConfig);
 
   const tsPath = path.join(process.cwd(), "tsconfig.json");
@@ -17,7 +37,19 @@ function withFrameworkConfig(defaultConfig = {}) {
 
   tsConfig.compilerOptions.paths["@framework/*"] = [`framework/${framework}/*`];
 
-  fs.writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2));
+  // fs.writeFileSync(
+  //   tsPath,
+  //   await prettier.format(JSON.stringify(tsConfig), { parser: "json" })
+  // );
+
+  try {
+    const formattedTsConfig = await prettier.format(JSON.stringify(tsConfig), {
+      parser: "json",
+    });
+    fs.writeFileSync(tsPath, formattedTsConfig);
+  } catch (error) {
+    console.error("Error writing to file: ", error);
+  }
 
   return config;
 }
